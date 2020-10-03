82556995e184becc2077d1a77c72246be5a7f94d
gustavobiel <56567873+gustavobiel@users.noreply.github.com>
2020-10-03 16:30:27 -0300

    generate universidade

---
+++ src/app/universidade/universidade.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-universidade',
+  templateUrl: './universidade.component.html',
+  styleUrls: ['./universidade.component.css']
+})
+export class UniversidadeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
